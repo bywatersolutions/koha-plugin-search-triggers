f022dd8a83a937054a9b7e2df6f17bf3e2bf4ca3
Kyle M Hall <kyle@bywatersolutions.com>
2018-07-26 11:15:09 +0000

    Initial commit

---
+++ Koha/Plugin/Com/ByWaterSolutions/IpWhiteList.pm
@@ -0,0 +1,83 @@
+package Koha::Plugin::Com::ByWaterSolutions::IpWhiteList;
+
+## It's good practive to use Modern::Perl
+use Modern::Perl;
+
+## Required for all plugins
+use base qw(Koha::Plugins::Base);
+
+## We will also need to include any Koha libraries we want to access
+use C4::Context;
+use C4::Members;
+use C4::Auth;
+use Koha::DateUtils;
+use Koha::Libraries;
+use Koha::Patron::Categories;
+use Koha::Account;
+use Koha::Account::Lines;
+use MARC::Record;
+use Cwd qw(abs_path);
+use URI::Escape qw(uri_unescape);
+use LWP::UserAgent;
+
+## Here we set our plugin version
+our $VERSION = "{VERSION}";
+
+## Here is our metadata, some keys are required, some are optional
+our $metadata = {
+    name            => 'IP Whitelist',
+    author          => 'Kyle M Hall',
+    date_authored   => '2009-01-27',
+    date_updated    => "1900-01-01",
+    minimum_version => '18.05.00.000',
+    maximum_version => undef,
+    version         => $VERSION,
+    description     => 'This plugin allows for managing a whitelist of IPs'
+};
+
+## This is the minimum code required for a plugin's 'new' method
+## More can be added, but none should be removed
+sub new {
+    my ( $class, $args ) = @_;
+
+    ## We need to add our metadata here so our base class can access it
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
+
+    ## Here, we call the 'new' method for our base class
+    ## This runs some additional magic and checking
+    ## and returns our actual $self
+    my $self = $class->SUPER::new($args);
+
+    return $self;
+}
+
+## If your tool is complicated enough to needs it's own setting/configuration
+## you will want to add a 'configure' method to your plugin like so.
+## Here I am throwing all the logic into the 'configure' method, but it could
+## be split up like the 'report' method is.
+sub configure {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    unless ( $cgi->param('save') ) {
+        my $template = $self->get_template({ file => 'configure.tt' });
+
+        ## Grab the values we already have for our settings, if any exist
+        $template->param(
+            ip_whitelist => $self->retrieve_data('ip_whitelist'),
+        );
+
+        $self->output_html( $template->output() );
+    }
+    else {
+        $self->store_data(
+            {
+                ip_whitelist => $cgi->param('ip_whitelist'),
+            }
+        );
+        $self->go_home();
+    }
+}
+
+1;
